9cae4171ca6eabf425871704b90213679df9a1c7
Francisco Cortes <beathouse@gmail.com>
2015-05-11 15:30:35 -0300

    creadas las relaciones en los modelos bpstest y tablas relacionadas

--- app/models/bpstest.rb
+++ app/models/bpstest.rb
@@ -1,3 +1,9 @@
 class Bpstest < ActiveRecord::Base
   belongs_to :user
+  has_many :cmp15_user_diseases
+  has_many :cmp161_surgeries
+  has_many :cmp191_medicines
+  has_many :cmp194_devices
+  has_many :cmp196_sleep_medicines
+  has_many :cmp199_af_medicines
 end
