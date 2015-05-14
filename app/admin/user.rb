8c10947ed31d58837e030721906542048f05b61d
Francisco Cortes <beathouse@gmail.com>
2015-05-14 15:16:35 -0300

    se ajusta active admin para administrar usuarios

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,23 +1,49 @@
 ActiveAdmin.register User do
-	permit_params :email, :password, :password_confirmation
+    config.comments = false
+    permit_params :name, :lastname, :email, :password, :password_confirmation, role_id:[]
  
     index do
+        column :name
+        column :lastname
         column :email
         column :current_sign_in_at
         column :last_sign_in_at
         column :sign_in_count
-        column :role
+        column :roles do |user|
+            user.roles.collect {|c| c.name }.to_sentence
+        end
         actions
     end
  
     filter :email
+
+    controller do
+        def create
+          @user = User.new(permitted_params[:user])
+          add_roles(@user)
+          create!
+        end
+
+        def update
+          add_roles(resource)
+          update!
+        end
+
+        private
+        def add_roles(resource)
+          resource.roles = []
+          params[:user][:role_ids].each { |r| resource.roles.push(Role.find(r)) unless r.blank? }
+        end
+    end
  
     form do |f|
         f.inputs "User Details" do
+            f.input :name
+            f.input :lastname
             f.input :email
             f.input :password
             f.input :password_confirmation
-            f.input :roles,  as: :check_boxes, :collection => Role.global
+            f.input :roles,  as: :check_boxes, collection: Role.all
         end
         f.actions
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511204215) do
+ActiveRecord::Schema.define(version: 20150514150654) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -392,6 +392,8 @@ ActiveRecord::Schema.define(version: 20150511204215) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "company_id"
+    t.string   "name"
+    t.string   "lastname"
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id"
