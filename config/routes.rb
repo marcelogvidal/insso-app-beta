32e2a8a931194e26cf0a593821d9781a6cda363f
linuxdevboot <rivascivil@gmail.com>
2015-09-10 11:52:02 -0300

    Gemfile update

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
 
   ActiveAdmin.routes(self)
-  
+
   root 'pages#index'
 
   #->Prelang (user_login:devise/stylized_paths)
